d778aee3685d15e74d8b432897cb2783726c1c70
Alan Bushell <alanbushell91@gmail.com>
2022-10-05 17:37:41 +0000

    add base template boilerplate, render first event, add event view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+
+class EventList(generic.ListView):
+    model = Event
+    queryset = Event.objects.filter(status=1).order_by('-event_added')
+    template_name = 'index.html'
+    paginate_by = 5
