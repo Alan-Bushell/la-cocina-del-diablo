8d9dc74305ad22249821480e3ecb137f462dbcbd
Alan Bushell <alanbushell91@gmail.com>
2022-10-05 18:24:48 +0000

    add event details pages

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.EventList.as_view(), name='home')
+    path('', views.EventList.as_view(), name='home'),
+    path('<slug:slug>', views.EventDetail.as_view(), name='event_detail'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 
@@ -8,3 +8,17 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('-event_added')
     template_name = 'index.html'
     paginate_by = 5
+
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": event,
+            })
