73f152feb9499149ef102a2d03fec4163d130a3c
Alan Bushell <alanbushell91@gmail.com>
2022-10-05 09:56:45 +0000

    restore settings.py

--- devils_kitchen/settings.py
+++ devils_kitchen/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['devils_kitchen.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['devils-kitchen.herokuapp.com', 'localhost']
 
 
 # Application definition
