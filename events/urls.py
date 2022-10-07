a5cfb536c57d868491b7a06d0c42963a3f49492e
Alan Bushell <alanbushell91@gmail.com>
2022-10-07 15:03:48 +0000

    added style to profile booking, fixed broken url pattern for events.

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.EventList.as_view(), name='events'),
     path('<slug:slug>', views.EventDetail.as_view(), name='event_detail'),
-    path('events/', views.events, name='events'),
 ]
