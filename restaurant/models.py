b615ef0ed894a18ef931237867e06477b85b5833
Alan Bushell <alanbushell91@gmail.com>
2022-10-22 12:27:45 +0000

    add users image to profile

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,6 +16,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField()
     contact_phone = models.CharField(max_length=15, null=False, blank=False)
+    profile_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
